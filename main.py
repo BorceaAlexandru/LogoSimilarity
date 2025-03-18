e624252032068fb7d7754bf20b065e6125f9aeb0
Borcea Alex <alxborcea@gmail.com>
2025-03-18 22:02:59 +0200

    first try getting the logos

--- main.py
+++ main.py
@@ -3,15 +3,25 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import json
+
 
 #config gecko
 service=Service("geckodriver.exe")
 options = Options()
 options.headless=True #rulare in fundal fara interfata grafica
+driver=webdriver.Firefox(service=service,options=options)
 
-driver = webdriver.Firefox(service=service,options=options)
+#adding protocol to each website
+def add_protocol(url):
+    if not url.startswith(("http://", "https://")):
+        return f"https://{url}"
+    return url
 
-#scot url din fisier
+#get urls from csv file
 def get_url(csv):
     if not os.path.exists(csv):
         print("File not found!")
@@ -29,8 +39,45 @@ def get_url(csv):
         print(f"Error reading CSV file: {e}")
         return []
 
-driver.get("https://www.google.com")
-driver.quit()
 
-urls=get_url("logos.csv")
-print(urls)
+#get logooos
+def get_logo(driver, url):
+    try:
+        driver.get(url)
+        #added timeout
+        logo_element=WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH,
+                                            '//img[contains(@class, "logo")]'))
+        )
+        logo_url=logo_element.get_attribute("src")
+        return logo_url
+    except Exception as e:
+        print(f"Error getting logo from {url}: {e}")
+        return None
+
+#main
+def main():
+    urls=get_url("logos.csv")
+    if not urls:
+        print("No URLs found!")
+        return
+
+    #all websites should have protocol
+    urls=[add_protocol(url) for url in urls]
+
+    logo_urls={}
+    for url in urls:
+        print("Processing URL:", url)
+        logo_url=get_logo(driver, url)
+        if logo_url:
+            logo_urls[url]=logo_url
+
+    driver.quit()
+
+    #save data in json file
+    with open("logo_urls.json","w") as f:
+        json.dump(logo_urls,f, indent=4)
+    print("Done!")
+
+if __name__=="__main__":
+    main()
